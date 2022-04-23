04327e4d3057fc6217ae7de8aaa4c3173635508e
Alex <s-alex-smir@yandex.ru>
2022-04-23 19:18:49 +0600

    added authorization

--- app/src/main/java/com/example/gamelist/MainActivity.java
+++ app/src/main/java/com/example/gamelist/MainActivity.java
@@ -144,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Toast.makeText(MainActivity.this, "Database error", Toast.LENGTH_LONG).show();
             }
         });
 
